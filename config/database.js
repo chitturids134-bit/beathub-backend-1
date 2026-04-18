ee87e4bc4ed4f811cfef27bfa9502a69456d9daa
Akhilan Theiventhiran <Akhilan@Akhilans-MacBook-Pro.local>
2026-04-18 09:53:55 +0530

    Handle legacy db_name in Mongo URI for Render deploys

--- config/database.js
+++ config/database.js
@@ -3,6 +3,25 @@ const mongoose = require("mongoose");
 let cachedConnection = null;
 let cachedConnectionPromise = null;
 
+const normalizeMongoUri = (mongoUri) => {
+  try {
+    const parsedUri = new URL(mongoUri);
+    const dbNameFromLegacyParam = parsedUri.searchParams.get("db_name");
+
+    if (dbNameFromLegacyParam) {
+      // Support legacy db_name while keeping compatibility with MongoDB driver options.
+      if (!parsedUri.pathname || parsedUri.pathname === "/") {
+        parsedUri.pathname = `/${dbNameFromLegacyParam}`;
+      }
+      parsedUri.searchParams.delete("db_name");
+    }
+
+    return parsedUri.toString();
+  } catch (_error) {
+    return mongoUri;
+  }
+};
+
 const connectToDatabase = async (mongoUri) => {
   if (!mongoUri) {
     throw new Error("MONGO_URI is not defined.");
@@ -17,10 +36,16 @@ const connectToDatabase = async (mongoUri) => {
       process.env.MONGO_SERVER_SELECTION_TIMEOUT_MS || "5000",
       10,
     );
+    const normalizedMongoUri = normalizeMongoUri(mongoUri);
 
     cachedConnectionPromise = mongoose
-      .connect(mongoUri, { serverSelectionTimeoutMS })
+      .connect(normalizedMongoUri, { serverSelectionTimeoutMS })
       .then((mongooseInstance) => mongooseInstance.connection);
+
+    cachedConnectionPromise = cachedConnectionPromise.catch((error) => {
+      cachedConnectionPromise = null;
+      throw error;
+    });
   }
 
   cachedConnection = await cachedConnectionPromise;
